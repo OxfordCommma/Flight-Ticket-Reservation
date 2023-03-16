28c0cbd4035bc73167ea74669f7137b0de19d438
Ramiro Farias <115428521+OxfordCommma@users.noreply.github.com>
2023-03-16 18:08:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,14 +8,25 @@ class Usuario:
         self.dni = int(input("Su DNI: "))
     
     def reservar_destino(self):
-        self.reserva_ida = input("Elija un destino del mundo: ")
-        return f"Destino elegido: {self.reserva_ida}"
+        
+        self.pais_salida = int(input("Indique el país de salida: \n1- Argentina\n2- Brasil\n3- Uruguay\n4- Peru\n5- Chile\n"))
+        self.pais_destino = int(input("Indique el país de destino: \n1- Argentina\n2- Brasil\n3- Uruguay\n4- Peru\n5- Chile\n"))
+        
+        while self.pais_salida not in range(1,6):
+            self.pais_salida = int(input("Valor desconocido: indique el país de salida: \n1- Argentina\n1- Brasil\n1- Uruguay\n1- Peru\n1- Chile"))
+        else:
+            while self.pais_destino not in range(1,6):
+                self.pais_destino = int(input("Valor desconocido: indique el país de destino: \n1- Argentina\n1- Brasil\n1- Uruguay\n1- Peru\n1- Chile"))
+            else:
+                return [self.pais_salida, self.pais_destino]
     
     def __str__(self):
-        return [f"Usuario - {self.nombre} {self.apellido}, {self.mail}, {self.telefono}, {self.dni} - {self.reservar_destino()}"]
+        self.diccionario = {1: 'Argentina', 2: 'Brasil', 3: 'Uruguay', 4: 'Perú', 5: 'Chile'}
+        self.a = self.reservar_destino()
+        return [f"Usuario - {self.nombre} {self.apellido}, {self.mail}, {self.telefono}, {self.dni} - Trayecto: {self.diccionario[self.a[0]]} hacia {self.diccionario[self.a[1]]}"]
     
     def resumen(self):
-        self.ticket_resumen = f"Usuario - {self.nombre} {self.apellido}, {self.mail}, {self.telefono}, {self.dni} - {self.reservar_destino()}"
+        self.ticket_resumen = f"Usuario - {self.nombre} {self.apellido}, {self.mail}, {self.telefono}, {self.dni} - Trayecto: {self.diccionario[self.a[0]]} hacia {self.diccionario[self.a[1]]}"
         return self.ticket_resumen
 
 
@@ -27,7 +38,18 @@ class Gestor_de_Tickets:
         return self.lista_usuarios
 
 
+class Vuelos:
+    import sqlite3
+
+    miConexion = sqlite3.connect("VuelosDisponiblesAbril")
+    miCursor = miConexion.cursor()
+
+    miConexion.commit()
+    miConexion.close()
+
+
 class Menu:
+    
 
     def run(self):
         iniciador = 0
@@ -40,7 +62,4 @@ class Menu:
             iniciador = int(input("Ingrese 0 para crear un nuevo usuario y reservar, o 99 para salir: "))
 
 
-Menu().run()    
-    
-        
-
+Menu().run()
